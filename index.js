51fe08f94d054ab1eb0c615a654fa49f03ef87a0
Gurpreet Atwal <gurpreetatwal@users.noreply.github.com>
2016-10-28 01:24:11 -0700

    wrap got to allow for servers or callback functions to be passed in

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const got = require('got');
 const http = require('http');
+const https = require('https');
 const urlParseLax = require('url-parse-lax');
 
-function normalizeArguments(url, opts) {
+function getPort(arg) {
+  const isSever = arg instanceof http.Server || arg instanceof https.Server;
+  const server = isSever ? arg : http.createServer(arg);
+
+  if (!server.listening) {
+    server.listen(0);
+  }
+
+  return server.address().port;
+}
+
+function normalizeArgs(url, options) {
   if (typeof url !== 'string' && typeof url !== 'object') {
     throw new Error(`Parameter \`url\` must be a string or object, not ${typeof url}`);
   }
@@ -16,42 +28,51 @@ function normalizeArguments(url, opts) {
     }
   }
 
-  return Object.assign(
-    {
-      protocol: 'http:',
-      path: '',
-      retries: 5
-    },
-    url,
-    opts
-  );
+  return Object.assign({
+    protocol: 'http:',
+    path: '',
+    retries: 5
+  }, url, opts);
 }
 
-function Got(url, options) {
-  try {
-    return got(normalizeArguments(url, opts));
-  } catch (err) {
-    return Promise.reject(err);
+module.exports = function request(arg) {
+
+  const port = getPort(arg);
+
+  function mergeDefault(url, options) {
+    return Object.assign({
+      hostname: '127.0.0.1',
+      port: port,
+    }, normalizeArgs(url, options));
   }
+
+  function Got(url, options) {
+    try {
+      return got(mergeDefault(url, options));
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  Got.stream = function(url, options) {
+    return got.stream(mergeDefault(url, options));
+  };
+
+  const helpers = [
+    'get',
+    'post',
+    'put',
+    'patch',
+    'head',
+    'delete',
+  ];
+
+  helpers.forEach(el => {
+    const setMethod = opts => Object.assign({}, opts, {method: el});
+
+    Got[el] = (url, opts) => Got(url, setMethod(opts));
+    Got.stream[el] = (url, opts) => Got.stream(url, setMethod(opts));
+  });
+
+  return Got;
 }
-
-const helpers = [
-  'get',
-  'post',
-  'put',
-  'patch',
-  'head',
-  'delete'
-];
-
-helpers.forEach(el => {
-  Got[el] = (url, opts) => Got(url, Object.assign({}, opts, {method: el}));
-});
-
-Got.stream = (url, opts) => Got.stream(normalizeArguments(url, opts));
-
-helpers.forEach(el => {
-  Got.stream[el] = (url, opts) => Got.stream(url, Object.assign({}, opts, {method: el}));
-});
-
-module.exports = Got;
