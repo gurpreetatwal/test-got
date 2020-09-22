fc9c59a01af486e8dffe4965d956cbaffc62da07
Gurpreet Atwal <git@gatwal.com>
2020-09-22 09:57:09 -0700

    fix(package): update dependencies

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
-import test from 'ava';
-import testGot from '.';
+'use strict';
+
+const test = require('ava');
+
+const testGot = require('.');
 
 test('basic', async t => {
   t.is(typeof testGot, 'function');
 
   const wrapped = testGot((req, res) => {
     res.write(req.url);
-    res.end()
+    res.end();
   });
   t.is(typeof wrapped, 'function');
   t.is(typeof wrapped.stream, 'function');
 
-  const helpers  = ['get', 'post', 'put', 'patch', 'delete'];
+  const helpers = ['get', 'post', 'put', 'patch', 'delete'];
   helpers.forEach(helper => {
     t.is(typeof wrapped[helper], 'function');
     t.is(typeof wrapped.stream[helper], 'function');
